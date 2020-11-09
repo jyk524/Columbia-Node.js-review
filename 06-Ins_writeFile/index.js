c638b1590b94ea4c8e07543f5c5a66fe487a47b7
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-08 23:17:52 -0500

    Writing files with node

--- 06-Ins_writeFile/index.js
+++ 06-Ins_writeFile/index.js
@@ -9,3 +9,6 @@ fs.writeFile("log.txt", process.argv[2], function(err) {
   console.log("Success!");
 
 });
+
+// In terminal enter in $node index.js {*arguement here*}
+  // this is to write the arguement into the log.txt
